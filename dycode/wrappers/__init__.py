cc316682465aabbb0ca6f830b68f56fce05b380e
hamidreza <hamidrezakamkari@gmail.com>
2022-12-28 13:10:56 -0500

    add full wrapper containing all the wrappers

--- dycode/wrappers/__init__.py
+++ dycode/wrappers/__init__.py
@@ -1,1 +1,5 @@
 from dycode.wrappers.method_wrapper import dynamic_method, dynamize_methods
+
+from dycode.wrappers.field_wrapper import dynamize_fields
+
+from dycode.wrappers.full_wrapper import sigless_wrapper as dynamize
